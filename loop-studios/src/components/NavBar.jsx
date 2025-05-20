ea9aa23b55661f01b395af3e6ba65dfed29c4c4c
zeez09 <harunaabdulazeez766@gmail.com>
2025-05-20 13:27:37 +0100

    added assets

--- loop-studios/src/components/NavBar.jsx
+++ loop-studios/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import hero from "../assets/images/image-hero.jpg";
+import logo from "../assets/images/logo.svg";
 
 
 
@@ -14,7 +15,17 @@ const NavBar = () => {
     <div className="relative w-full">
       <img src={hero} alt="" className="w-screen" />
 
-      <nav className="absolute top-0 left-0 w-full flex items-center justify-between text-white max-w-7xl px-8 py-3">
+      <nav className="absolute top-0 left-0 w-full flex items-center justify-between text-white max-w-7xl px-30 py-15">
+        <div>
+          <img src={logo} alt="" />        </div>
+          <ul className="hidden sm:flex list-none gap-6 text-sm text-gray-500">
+        {navLinks.map((link, index) => (
+    <li key={index} className="hover:underline cursor-pointer capitalize">
+        {link}
+    </li>
+))}
+</ul>
+
         
 
         
