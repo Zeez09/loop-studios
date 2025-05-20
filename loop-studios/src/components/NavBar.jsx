cf7b60ce54ed2c68ac1fb3f4bb59a1c7f24dd587
zeez09 <harunaabdulazeez766@gmail.com>
2025-05-20 11:48:18 +0100

    second commit

---
+++ loop-studios/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+
+
+
+const NavBar = () => {
+  return (
+    <>
+    
+    <div className="w-full p-4 border-b border-gray-200">
+      <nav className=""> 
+        
+      </nav>
+
+
+      
+    </div>
+    
+    </>
+  )
+}
+
+export default NavBar
