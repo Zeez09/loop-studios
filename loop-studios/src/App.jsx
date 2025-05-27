7c34cd5c939e1bd2389827bfc6ce88408060d20a
zeez09 <harunaabdulazeez766@gmail.com>
2025-05-27 13:01:49 +0100

    finishing touches

--- loop-studios/src/App.jsx
+++ loop-studios/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from './components/NavBar'
 import './App.css'
 import interactive from "./assets/images/mobile/image-interactive.jpg"
@@ -21,7 +21,11 @@ import fish from "./assets/images/mobile/image-fisheye.jpg"
 import fish2 from "./assets/images/Desktop/image-fisheye.jpg"
 import Footer from "./components/Footer"
 
+
 function App() {
+
+  
+
   
 
   return (
@@ -30,35 +34,43 @@ function App() {
 
 <NavBar />
 
-<div className='px-36 py-16'>
+<div className='lg:px-36 py-16 px-4 sm:px-4'>
 
-<div className=' mt-10 flex flex-row'>
-  <div className='relative'>
-  <img src={interactive} alt=""  className='block sm:hidden'/>
-  <img src={interactive2} alt=""  className='hidden sm:block'/>
-  <div className="absolute bottom-0 left-1/3 translate-x-1/2 bg-white p-6 sm:p-10 w-[550px] text-black">
+<div className="mt-10 flex flex-col sm:flex-row items-center sm:items-start relative w-full">
+  < div className='w-full sm:w-1/2 relative'>
+  <img src={interactive} alt=""  className='block sm:hidden w-full'/>
+  <img src={interactive2} alt=""  className='hidden sm:block w-full'/>
   
-  <div className='flex flex-col'>
-  <h1 className='text-3xl font-light px-10'>
+  
+  <div className="bg-white p-6 sm:p-10 text-black mt-4 sm:mt-0 sm:absolute sm:bottom-0 sm:right-0 sm:w-[550px] sm:translate-x-2/3 sm:translate-y-2/3">
+  
+
+  <h1 className="text-3xl font-light text-center sm:text-left px-4 sm:px-0">
     THE LEADER IN<br/>INTERACTIVE VR
   </h1>
-  <p className='text-xs mt-10 px-10 font-extralight'>Founded in 2011, Loopstudios has been producing world-class<br/>
+<p className="text-xs mt-6 font-extralight text-center sm:text-left px-4 sm:px-0">
   virtual reality projects for some of the best companies around the<br/>
   globe. Our award-winning creations have transformed businesses<br/>
   through digital experiences that bind to their brand.
 
   </p>
-  </div>
+  
   
   </div>
+  </div>
+
+
 
 </div>
-</div>
 
-<div className='flex flex-row justify-between py-18'>
-  <h1 className='font-light text-3xl'>OUR CREATIONS</h1>
-  <button className='text-black bg-transparent hover:text-white hover:bg-black px-10 border border-black'>SEE ALL</button>
-</div>
+ <div className="flex justify-between items-center lg:py-10">
+  <h1 className="font-light text-3xl text-center lg:text-left w-full lg:w-auto">
+    OUR CREATIONS
+  </h1>
+          <button className="hidden sm:inline-block text-black bg-transparent hover:text-white hover:bg-black px-10 py-2 border border-black">
+            SEE ALL
+          </button>
+        </div>
 
 <div className='flex flex-col '>
       <div className=''>
@@ -72,28 +84,77 @@ function App() {
       <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
     </div>
   </div>
-  <img src={night} alt="" className='block sm:hidden'/>
-  <img src={night2} alt="" className='hidden sm:block'/>
-  <img src={soccer} alt="" className='block sm:hidden'/>
-  <img src={soccer2} alt="" className='hidden sm:block'/>
-  <img src={grid} alt="" className='block sm:hidden'/>
-  <img src={grid2} alt="" className='hidden sm:block'/>
+
+  <div className="relative overflow-hidden">
+  <img src={night} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={night2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
+  
+  <div className="relative overflow-hidden">
+  <img src={soccer} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={soccer2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
+  <div className="relative overflow-hidden">
+  <img src={grid} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={grid2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
   
 
 </div>
 
-<div className='grid grid-cols-1 sm:grid-cols-4 gap-10 mt-5'>
-  <img src={above} alt="" className='block sm:hidden'/>
-  <img src={above2} alt="" className='hidden sm:block'/>
-  <img src={pocket} alt="" className='block sm:hidden'/>
-  <img src={pocket2} alt="" className='hidden sm:block'/>
-  <img src={curiosity} alt="" className='block sm:hidden'/>
-  <img src={curiosity2} alt="" className='hidden sm:block' />
-  <img src={fish} alt="" className='block sm:hidden'/>
-  <img src={fish2} alt="" className='hidden sm:block'/>
-  
+<div className='grid grid-cols-1 sm:grid-cols-4 gap-1 mt-5'>
+  <div className="relative overflow-hidden">
+  <img src={above} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={above2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
+  <div className="relative overflow-hidden">
+  <img src={pocket} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={pocket2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
+  <div className="relative overflow-hidden">
+  <img src={curiosity} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={curiosity2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
+  <div className="relative overflow-hidden">
+  <img src={fish} alt=""  className='block sm:hidden w-full h-full object-cover'/>
+  <img src={fish2} alt=""  className='hidden sm:block w-full h-full object-cover'/>
+  <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+  <div className="absolute bottom-4 left-4 text-white text-lg sm:text-xl font-bold">
+      <h1 className='font-extralight px-5'>DEEP <br/> EARTH</h1>
+    </div>
+  </div>
 
 </div>
+
+<div className="block sm:hidden mt-6 w-full text-center">
+          <button className="text-black bg-transparent hover:text-white hover:bg-black px-10 py-2 border border-black">
+            SEE ALL
+          </button>
+        </div>
 
 </div>
 </div>
--- loop-studios/src/components/NavBar.jsx
+++ loop-studios/src/components/NavBar.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import hero from "../assets/images/mobile/image-hero.jpg";
 import hero2 from "../assets/images/Desktop/image-hero.jpg";
 import logo from "../assets/images/Desktop/logo.svg";
+import Hamburger from 'hamburger-react'
+import icon from "../assets/images/Desktop/icon-close.svg";
 
 
 
 
 
-const navLinks = ["About", "Careers", "Events", "Products", "Support"]
+
 
 const NavBar = () => {
+  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navLinks = ["About", "Careers", "Events", "Products", "Support"]
+
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
+  const closeSidebar = () => setSidebarOpen(false);
+
+
+
   return (
     <>
     
     <div className="relative w-full">
-      <img src={hero} alt="" className="w-screen block sm:hidden" />
+      <img src={hero} alt="" className=" block sm:hidden w-full" />
       <img src={hero2} alt="" className="w-screen hidden sm:block" />
 
-      <nav className="absolute top-0 left-0 w-full flex items-center justify-between text-white  px-36 py-18">
-        <div>
-          <img src={logo} alt="" /></div>
+<nav className="absolute top-0 left-0 w-full flex items-center justify-between text-white px-6 sm:px-20 py-6">
+
+        <div className={`${sidebarOpen ? "hidden" : "block"} sm:block`}>
+
+          <img src={logo} alt="" />
+          </div>
           <ul className=" hidden sm:flex list-none gap-6 josefin-sans-300 text-white font-bold">
         {navLinks.map((link, index) => (
     <li key={index} className="inline cursor-pointer capitalize hover:underline hover:decoration-2 hover:underline-offset-4"
@@ -30,17 +44,37 @@ const NavBar = () => {
 </ul>
 
 
+<div className="sm:hidden">
+  <Hamburger toggled={sidebarOpen} toggle={toggleSidebar} />
+        </div>
+
+</nav>
+
+{sidebarOpen && (
+  <div className="fixed top-0 left-0 w-[70%] bg-black bg-opacity-90 p-6 z-40 sm:hidden">
+    <div className="flex items-center justify-between mb-8 flex-row">
+        <img src={logo} alt="" className="h-6" />
+        <button className="text-amber-50 " onClick={closeSidebar}>X</button>
+
+      </div>
+    <ul className="flex flex-col gap-6 text-white font-bold mt-30">
+        {navLinks.map((link, index) => (
+    <li key={index} onClick={closeSidebar} className="inline cursor-pointer capitalize hover:underline hover:decoration-2 hover:underline-offset-4"
+>
+        {link}
+    </li>
+        ))}
+    </ul>
+
+    </div>
+
+)}
 
 
-        
 
-        
-
-      </nav>
-
-      <div className="absolute top-24 w-full text-white  px-36 py-18">
-        <div className=" p-6 sm:p-10 rounded-lg w-[800px] border border-white mt-10">
-        <h1 className="text-7xl sm:text-7xl max-w-5xl">
+      <div className="absolute top-24 w-full text-white px-6 sm:px-36">
+        <div className="p-4 sm:p-10 rounded-lg w-full sm:w-[800px] border border-white mt-10">
+          <h1 className="text-4xl sm:text-7xl leading-tight">
           IMMERSIVE<br/>EXPERIENCES<br/>THAT DELIVER
         </h1>
         
